6b42d3e66fde450f94114b579d5f60f7a0a81c00
edwintantawi <edwintantawi@gmail.com>
2021-03-08 20:53:31 +0700

    add mainchat header, chat area and footer

--- src/components/MainChat/index.jsx
+++ src/components/MainChat/index.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import avatarPicture from 'assets/images/avatar.png';
+import MoodIcon from '@material-ui/icons/Mood';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
 import './index.scss';
 function MainChat() {
   return (
     <div className='mainchat'>
-      <h1>Main Chat</h1>
+      <div className='mainchat__header'>
+        <div className='mainchat__header__title'>
+          <Avatar alt='Your Avatar' src={avatarPicture} />
+          <div className='mainchat__header__title__text'>
+            <span className='mainchat__header__title__text__name'>
+              Lets Build a Futures
+            </span>
+            <span className='mainchat__header__title__text__description'>
+              Me, 0828347912234, 823174902317, 238174092319
+            </span>
+          </div>
+        </div>
+        <div className='mainchat__header__icons'>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='mainchat__chatarea'></div>
+
+      <div className='mainchat__footer'>
+        <div className='mainchat__footer__lefticon'>
+          <IconButton>
+            <MoodIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+        </div>
+        <div className='mainchat__footer__input'></div>
+        <div className='mainchat__footer__righticon'></div>
+      </div>
     </div>
   );
 }
