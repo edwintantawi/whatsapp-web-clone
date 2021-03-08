114cdccf673d76eb1154cc5e7682fd8e2de04329
edwintantawi <edwintantawi@gmail.com>
2021-03-08 19:10:33 +0700

    added sideChat header

--- src/components/SideChat/index.jsx
+++ src/components/SideChat/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import avatarPicture from 'assets/images/avatar.png';
 import './index.scss';
+
 function SideChat() {
   return (
     <div className='sidechat'>
-      <h1>SideChat</h1>
+      <div className='sidechat__header'>
+        <Avatar alt='Remy Sharp' src={avatarPicture} />
+        <div className='sidechat__header__icons'>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className='sidechat__search'></div>
+      <div className='sidebar__chatlist'></div>
     </div>
   );
 }
